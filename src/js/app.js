cbf6787f8058b0c3b68c04f823624ff8fafd1220
Sayah Reda <reda1998.rs@gmail.com>
2022-03-05 14:28:23 +0100

    Split code using lazy and suspense

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import '../css/style.css';
 import '../css/crew.css';
 import '../css/technology.css';
 import { render } from 'react-dom';
 import logo from '../imgs/shared/logo.svg';
-import Destination from './destination';
-import Technology from './technology';
-import Crew from './crew';
-import Home from './home';
+const Destination = lazy(() => import('./destination'));
+const Technology = lazy(() => import('./technology'));
+const Crew = lazy(() => import('./crew'));
 
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Home from './home';
+import Loader from './loader';
+
+import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom';
 const App = () => {
   const openandclose = () => {
     console.log('hello');
@@ -19,92 +21,94 @@ const App = () => {
     document.getElementById('spinner').classList.toggle('menuclicked');
   };
   return (
-    <Router>
-      <div>
-        <nav>
-          <Link to="/">
-            <img src={logo} className="logo" />
-          </Link>
-          <div className="co_nav">
-            <div className="line"></div>
-            <ul className="navbar">
-              <li>
-                <Link to="/">
-                  <a>
-                    <span>00 </span> HOME
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link to="/destination">
-                  <a>
-                    <span>01 </span> DESTINATION
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link to="/crew">
-                  <a>
-                    <span>02 </span> CREW
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link to="/technology">
-                  <a>
-                    <span>03 </span> TECHNOLOGY
-                  </a>
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="hamburger">
-            <div
-              className="menu menunoclicked"
-              id="spinner"
-              onClick={openandclose}
-            ></div>
-            <ul className="res_navbar close_navbar" id="menu">
-              <li>
-                <Link to="/">
-                  <a>
-                    <span>00 </span>HOME
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link to="/destination">
-                  <a>
-                    <span>01 </span>DESTINATION
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link to="/crew">
-                  <a>
-                    <span>02 </span>CREW
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link to="/technology">
-                  <a>
-                    <span>03 </span>TECHNOLOGY
-                  </a>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
+    <Suspense fallback={Loader}>
+      <Router>
+        <div>
+          <nav>
+            <Link to="/">
+              <img src={logo} className="logo" />
+            </Link>
+            <div className="co_nav">
+              <div className="line"></div>
+              <ul className="navbar">
+                <li>
+                  <Link to="/">
+                    <a>
+                      <span>00 </span> HOME
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/destination">
+                    <a>
+                      <span>01 </span> DESTINATION
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/crew">
+                    <a>
+                      <span>02 </span> CREW
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/technology">
+                    <a>
+                      <span>03 </span> TECHNOLOGY
+                    </a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="hamburger">
+              <div
+                className="menu menunoclicked"
+                id="spinner"
+                onClick={openandclose}
+              ></div>
+              <ul className="res_navbar close_navbar" id="menu">
+                <li>
+                  <Link to="/">
+                    <a>
+                      <span>00 </span>HOME
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/destination">
+                    <a>
+                      <span>01 </span>DESTINATION
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/crew">
+                    <a>
+                      <span>02 </span>CREW
+                    </a>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/technology">
+                    <a>
+                      <span>03 </span>TECHNOLOGY
+                    </a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
 
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/destination" component={Destination} />
-          <Route exact path="/crew" component={Crew} />
-          <Route exact path="/technology" component={Technology} />
-        </Switch>
-      </div>
-    </Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/destination" component={Destination} />
+            <Route exact path="/crew" component={Crew} />
+            <Route exact path="/technology" component={Technology} />
+          </Switch>
+        </div>
+      </Router>
+    </Suspense>
   );
 };
 
---
+++ src/js/loader.js
@@ -0,0 +1,11 @@
+const loader = () => {
+  return (
+    <div class="loader">
+      <span></span>
+      <span></span>
+      <span></span>
+      <h2>Loading....</h2>
+    </div>
+  );
+};
+export default loader;
